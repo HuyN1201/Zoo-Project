582906478793f9a3cd9b825ce1b360e62ed7604c
Huy Nguyen <huyn120198@gmail.com>
2021-04-25 19:56:05 -0500

    Create

---
+++ create.sql
@@ -0,0 +1,106 @@
+-- Drop tables if they already exist so that they can get recreated.
+-- Drop the referencing tables first to respect referential integrity.
+-- Notice that we get warnings if we try to drop tables that don't exist.
+DROP TABLE IF EXISTS Building_Information;
+DROP TABLE IF EXISTS Staff_Information;
+DROP TABLE IF EXISTS Animal_Information;
+DROP TABLE IF EXISTS Animal_Exhibit_Information;
+DROP TABLE IF EXISTS Animal_Schedule_Informaiton;
+DROP TABLE IF EXISTS Trainer;
+DROP TABLE IF EXISTS Participates;
+DROP TABLE IF EXISTS Location;
+DROP TABLE IF EXISTS Feeding;
+DROP TABLE IF EXISTS On_the_Loose;
+
+CREATE TABLE Building_Information ( 
+	buildingID		CHAR(4), 
+	bName			VARCHAR(20), 
+	description		VARCHAR(1000), 
+	forExhibit		BOOLEAN, 
+
+	PRIMARY KEY (buildingID) 
+);
+
+CREATE TABLE Staff_Information ( 
+	staffID			CHAR(5)	NOT NULL, 
+	FirstName		VARCHAR(10), 
+	LastName		VARCHAR(10), 
+	ClockedIn		BOOLEAN, 
+
+	PRIMARY KEY (staffID) 
+);
+
+CREATE TABLE Animal_Information ( 
+	animalID	CHAR(5)	NOT NULL, 
+	aName		VARCHAR(10), 
+	Species		VARCHAR(20), 
+	FoodType	VARCHAR(20), 
+
+	PRIMARY KEY (animalID)
+); 
+
+CREATE TABLE Trainer ( 
+	staffID		CHAR(5) 	NOT NULL, 
+	animalID	CHAR(5)		NOT NULL, 
+
+	PRIMARY KEY (animalID, staffID), 
+	FOREIGN KEY (animalID) REFERENCES Animal_Information(animalID),
+	FOREIGN KEY (staffID) REFERENCES Staff_Information(staffID) 
+); 
+
+CREATE TABLE Animal_Exhibit_Information ( 
+	exhibitID	CHAR(10) NOT NULL, 
+	eName		VARCHAR(25) NOT NULL,	 
+	vacancy		BOOLEAN, 
+	DateStart 	DATE, 
+	DateEnd		DATE, 
+
+	PRIMARY KEY (exhibitID) 
+); 
+
+CREATE TABLE Participates ( 
+	exhibitID	CHAR(10) NOT NULL, 
+	animalID	CHAR(5)	NOT NULL, 
+
+	PRIMARY KEY (exhibitID, animalID), 
+	FOREIGN KEY (exhibitID) REFERENCES Animal_Exhibit_Information(exhibitID), 
+	FOREIGN KEY (animalID) REFERENCES Animal_Information(animalID) 
+); 
+
+CREATE TABLE Location ( 
+	exhibitID	CHAR(10) NOT NULL, 
+	buildingID	CHAR(4), 
+
+	PRIMARY KEY (exhibitID,  buildingID), 
+	FOREIGN KEY (exhibitID) REFERENCES Animal_Exhibit_Information(exhibitID), 
+	FOREIGN KEY (buildingID) REFERENCES Building_Information(buildingID) 
+); 
+
+CREATE TABLE Animal_Schedule_Informaiton ( 
+	feedingTime	DATETIME, 
+	wakeTime	TIME, 
+	sleepTime	TIME, 
+
+	PRIMARY KEY (feedingTime) 
+); 
+
+CREATE TABLE Feeding ( 
+	animalID 	CHAR(5)		NOT NULL, 
+	feedingTime	DATETIME, 
+    
+	PRIMARY KEY (animalID,  feedingTime),
+	FOREIGN KEY (animalID) REFERENCES Animal_Information(animalID),
+	FOREIGN KEY (feedingTime) REFERENCES Animal_Schedule_Information(feedingTime) 
+); 
+
+CREATE TABLE On_the_Loose ( 
+	animalID	CHAR(5)  	NOT NULL, 
+	exhibitID	CHAR(10)  	NOT NULL, 
+	captured	BOOLEAN     NOT NULL, 
+
+	PRIMARY KEY (animalID),  
+	FOREIGN KEY (animalID) REFERENCES Animal_Information(animalID) ON DELETE CASCADE ON UPDATE CASCADE, 
+	FOREIGN KEY (exhibitID) REFERENCES Animal_Exhibit_Information(exhibitID)
+); 
+
+  
